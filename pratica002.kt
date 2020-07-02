6f57d286713da3f36ab0cf4983ed2db2aa806f88
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-07-01 23:10:19 -0300

    create pratica

---
+++ pratica002.kt
@@ -0,0 +1,11 @@
+/**
+ * Variáveis
+ */
+
+fun main() {
+    var nome:String = "Lucas"
+    var idade:Short = 32 // Pode ser declarada com Byte (até 127), Short (até 32000) ou Int
+    var peso:Float = 81.8F
+    var numGrande:Long = 5_000_000L
+    print(nome)
+}
