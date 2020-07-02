4711a46ab5c3da6f47753c2c62d6675fdbba1a67
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-07-01 23:11:01 -0300

    create pratica

---
+++ pratica003.kt
@@ -0,0 +1,11 @@
+/**
+ * Constante
+ */
+fun main() {
+    val nome:String = "Lucas"
+    val idade:Int
+    idade = 32 // atribuição pós declaração (ok)
+    //nome = "Rafael" <-- Não é possível reatribuir uma constante
+    //idade = 30 <-- constante já foi atribuída
+    print(idade)
+}
