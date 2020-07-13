f2d8e6f5ba4a2ede1e87c11d844c7a826ce99e77
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-07-13 03:09:02 -0300

    create pratica013

---
+++ pratica013.kt
@@ -0,0 +1,11 @@
+import kotlin.math.* // importa toda biblioteca .math
+
+fun main() {
+    println("raiz quadrada: ${sqrt(25f)}")
+    println("potência: ${7f.pow(2)}")
+    println("arredonda para cima: ${ceil(8.1f)}")
+    println("arredonda inteiro: ${8.5f.roundToInt()}")
+    println("arredonda para baixo: ${floor(8.8f)}")
+    println("maior: ${max(7,9)}")
+    println("menor: ${min(7,9)}")
+}
