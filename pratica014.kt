46bbe57dca7b60bfb58a2d9b2d6cf80c8a59b561
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-07-13 03:09:38 -0300

    new create

---
+++ pratica014.kt
@@ -0,0 +1,13 @@
+/**
+ * Operadores relacionais
+ */
+fun main() {
+    var n1:Int = 5
+    var n2:Int = 4
+    println("$n1 é maior que $n2? ${n1>n2}")
+    println("$n1 é menor que $n2? ${n1<n2}")
+    println("$n1 é maior ou igual que $n2? ${n1>=n2}")
+    println("$n1 é menor ou igual que $n2? ${n1<=n2}")
+    println("$n1 é igual a $n2? ${n1==n2}")
+    println("$n1 é diferente que $n2? ${n1!=n2}")
+}
