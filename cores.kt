a4af6f63804b8a6efe105ba458f15de68417894a
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-07-16 23:45:21 -0300

    create cores

---
+++ cores.kt
@@ -0,0 +1,11 @@
+package exercicios
+// Código para cores
+val reset:String = "\u001B[m"
+val preto:String = "\u001B[30m"
+val vermelho:String = "\u001B[31m"
+val verde:String = "\u001B[32m"
+val amarelo:String = "\u001B[33m"
+val azul:String = "\u001B[34m"
+val magenta:String = "\u001B[35m"
+val ciano:String = "\u001B[36m"
+val branco:String = "\u001B[37m"
